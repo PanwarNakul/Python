e1e92be6c13ba8f30bf62d6764c9ce20e053ba13
PanwarNakul <nakul.panwar77@gmail.com>
2017-04-24 19:10:09 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+##Numbers
+
+Var1=10
+print Var1
+#del Var1
+print Var1
+
+##Stings
+Str='"this is test string"'
+print Str;
+print Str[2:];
+print Str*2;
+print Str+'Nakul';
+
+##List
+list=['nakul', 78, 'Ankit',45];
+print list;
+tuple=('nakul', 78, 'Ankit',45)
+#tuple[2]=98
+list[2]=98
+print list
