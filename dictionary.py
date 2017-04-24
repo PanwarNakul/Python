9a41b8b4857a9711238c1c3a0309e9945aa6f3ae
PanwarNakul <nakul.panwar77@gmail.com>
2017-04-24 19:57:30 +0530

    Create dictionary.py
    In built dictionary data type in Python

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+dict={}
+dict[1]="this is first element";
+dict[2]="this is second element";
+print dict[1];
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+print tinydict;
+print tinydict['name'];
+print tinydict.keys();
+print tinydict.values();
